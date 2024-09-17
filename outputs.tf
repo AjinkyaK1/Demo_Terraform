61eb8e09e5bdcfddfa0679f10791426dc02eb2d5
AjinkyaK1 <ajinkya.kale67@yahoo.in>
2024-09-17 23:08:45 +0530

    outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,28 @@
+output "s3_bucket_id" 
+{
+  value = aws_s3_bucket.private_bucket.id
+}
+
+output "efs_file_system_id" 
+{
+  value = aws_efs_file_system.app_efs.id
+}
+
+output "ec2_instance_id" 
+{
+  value = aws_instance.app_server.id
+}
+
+output "security_group_id" 
+{
+  value = aws_security_group.web_sg.id
+}
+
+output "subnet_id" 
+{
+  value = aws_subnet.public_subnet.id
+}
+
+
+
+
