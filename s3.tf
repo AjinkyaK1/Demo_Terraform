63d070b6db5e37176795419878060cc068e3e619
AjinkyaK1 <ajinkya.kale67@yahoo.in>
2024-09-17 23:05:24 +0530

    s3.tf

---
+++ s3.tf
@@ -0,0 +1,10 @@
+# Creating an S3 bucket with private access
+resource "aws_s3_bucket" "private_bucket" 
+{
+  bucket_prefix = "NetSPI-dev-private-bucket-"
+  acl = "private"
+  tags = 
+  {
+    Name = "PrivateBucket"
+  }
+}
