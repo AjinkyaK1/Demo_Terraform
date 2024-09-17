fb4876a9231102b4749b895a2fd6f970b2423138
AjinkyaK1 <ajinkya.kale67@yahoo.in>
2024-09-17 23:06:31 +0530

    ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,31 @@
+# Launch an EC2 instance
+resource "aws_instance" "app_server" 
+{
+  ami = "ami-0abcdef1234567890"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.web_sg.name]
+  tags = 
+  {
+    Name = "AppServer"
+  }
+
+  # Remote-exec Provisioner to set up the instance
+  provisioner "remote-exec" 
+  
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y nfs-utils",
+      "sudo mkdir -p /data/test",
+      "sudo mount -t nfs ${aws_efs_file_system.app_efs.dns_name}:/ /data/test",
+      "echo '${aws_efs_file_system.app_efs.dns_name}:/ /data/test nfs defaults 0 0' | sudo tee -a /etc/fstab"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/key.pem")
+      host        = self.public_ip
+    }
+  }
+}
