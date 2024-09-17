b89885de4fc87d842fcd80a9f71b84414ed22e1e
AjinkyaK1 <ajinkya.kale67@yahoo.in>
2024-09-17 23:04:15 +0530

    subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,13 @@
+# subnet.tf
+# Defining a Public subnet within the VPC
+resource "aws_subnet" "public_subnet" 
+{
+  vpc_id = aws_vpc.dev_vpc.id
+  cidr_block = "10.1.1.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+  tags = 
+  {
+    Name = "PublicSubnet"
+  }
+}
